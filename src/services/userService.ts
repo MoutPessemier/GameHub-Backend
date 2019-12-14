d1b9145a3c8fd85c6bb488a8d9dc190da0ac4cf6
Mout Pessemier <moutpessemier@hotmail.com>
2019-12-14 16:30:00 +0100

    V1.1.12 - [HOTFIX] - updateUser

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -29,21 +29,11 @@ routes.get('/getUserByEmail', async (req, res) => {
 // Athentication is handled by Auth0, this is used to sync users with Auth0
 
 routes.post('/register', async (req, res) => {
-  // const errors = { email: '', phone: '', birthDate: '' };
-  // if (!validateEmail(req.body.email)) errors.email = '';
-  // if (!validatePhone(req.body.phone)) errors.phone = '';
-  // if (!isOldEnough(stringToDate(req.body.birthDate))) errors.birthDate = '';
-  // if (errors.email !== '' || errors.phone !== '' || errors.birthDate !== '')
-  // return res.sendStatus(400).send({ errors });
-  //const hashedPass = await hashPassword(req.body.password);
   const user = await models.user.model.create({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    //telephone: req.body.telephone,
     email: req.body.email,
-    //birthDate: stringToDate(req.body.birthDate),
     userRole: 'USER',
-    //password: hashedPass,
     maxDistance: 10
   });
   return res.send({ user });
@@ -52,32 +42,18 @@ routes.post('/register', async (req, res) => {
 routes.post('/login', async (req, res) => {
   const user = await models.user.model.findOne({ email: req.body.email });
   if (!user) return res.sendStatus(400).send({ error: `No user found with email: ${req.body.email}.` });
-  //const isEqual = await comparePasswords(req.body.password, user.password);
-  // if (!isEqual) {
-  //   return res.send({ error: `Passwords does not match, try again.` });
-  // }
   return res.send({ user });
 });
 
 routes.put('/updateUser', async (req, res) => {
-  // const errors = { email: '', phone: '', birthDate: '' };
-  // if (!validateEmail(req.body.email)) errors.email = '';
-  //if (!validatePhone(req.body.phone)) errors.phone = '';
-  //if (!isOldEnough(dateFix(req.body.birthDate))) errors.birthDate = '';
-  // if (errors.email !== '' || errors.phone !== '' || errors.birthDate !== '')
-  // return res.sendStatus(406).send({ errors });
-  //const hashedPass = await hashPassword(req.body.password);
-  const updatedUser = await models.user.model
+  const user = await models.user.model
     .findByIdAndUpdate(
       { _id: req.body._id },
       {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
-        //telephone: req.body.telephone,
         email: req.body.email,
-        //birthDate: dateFix(req.body.birthDate),
         userRole: req.body.userRole,
-        //password: hashedPass,
         maxDistance: req.body.maxDistance
       },
       { new: true, upsert: true }
@@ -86,7 +62,7 @@ routes.put('/updateUser', async (req, res) => {
       Sentry.captureException(e);
       return res.sendStatus(404).send(e);
     });
-  return res.send({ updatedUser });
+  return res.send({ user });
 });
 
 routes.delete('/deleteUser', async (req, res) => {
